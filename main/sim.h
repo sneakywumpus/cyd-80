fbe35e5bbef75da9c88022fbb6c87e9d45a075c7
Thomas Eberhardt <sneakywumpus@gmail.com>
2024-12-14 15:05:52 +0100

    updates from z80pack/picosim, remove LovyanGFX component

--- main/sim.h
+++ main/sim.h
@@ -14,15 +14,18 @@
 #define CPU_SPEED 4	/* CPU speed 0=unlimited */
 /*#define ALT_I8080*/	/* use alt. 8080 sim. primarily optimized for size */
 /*#define ALT_Z80*/	/* use alt. Z80 sim. primarily optimized for size */
-#define UNDOC_INST	/* compile undocumented instructions */
+#define UNDOC_INST	/* compile undocumented instrs. (required by ALT_*) */
+#ifndef EXCLUDE_Z80
 /*#define FAST_BLOCK*/	/* much faster but not accurate Z80 block instr. */
+#endif
 #define BAREMETAL	/* set up the simulator core for bare metal use */
 
-/*#define WANT_ICE*/	/* attach ICE to headless machine */
+#define WANT_ICE	/* attach ICE to headless machine */
 #ifdef WANT_ICE
 #define WANT_TIM	/* count t-states */
 #define HISIZE	100	/* number of entries in history */
 #define SBSIZE	4	/* number of software breakpoints */
+#define WANT_HB		/* hardware breakpoint */
 #endif
 
 #define USR_COM "ESP32-2432S028R Z80/8080 emulator"
